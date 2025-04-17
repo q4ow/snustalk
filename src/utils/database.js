3564836e6d4a8db4775f9a6992b2b771742927ee
Keiran <keiran0@proton.me>
2025-04-17 04:52:31 +0100

    missing import

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,6 +3,7 @@ const { Pool } = pkg;
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import crypto from "crypto";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
